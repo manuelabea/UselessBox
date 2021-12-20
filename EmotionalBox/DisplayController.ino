81f6eab17125f6f0334a0b2ba11afdc08ce1ec50
LowLevelSubmarine <florian.weichert@unitybox.de>
2021-12-20 12:19:26 +0100

    Added e-ink libraries and a bit of project structure

---
+++ EmotionalBox/DisplayController.ino
@@ -0,0 +1,7 @@
+// E-Ink display library
+// https://github.com/soonuse/epd-library-arduino/tree/master/4.2inch_e-paper_b/epd4in2b
+#include <fonts.h>
+#include <epd4in2b.h>
+#include <epdpaint.h>
+#include <epdif.h>
+#include <epd2in13b.h>
